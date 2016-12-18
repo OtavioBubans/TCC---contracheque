0e442782c4da5809233641b8da086097fa669963
Matheus Schmitz <augs.math@gmail.com>
2016-12-18 16:01:48 -0200

    feat(Logger): Mover para camada de infraestrutura e adicionar dependencia à mesma para a camada de serviços.

--- contra-cheque/contra-cheque-infrastructure/src/main/java/br/com/crescer/contra/cheque/infrastructure/Logger.java
+++ contra-cheque/contra-cheque-infrastructure/src/main/java/br/com/crescer/contra/cheque/infrastructure/Logger.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.crescer.contra.cheque.services;
+package br.com.crescer.contra.cheque.infrastructure;
 
 import br.com.crescer.contra.cheque.entity.Colaborador;
 import br.com.crescer.contra.cheque.entity.Log;
