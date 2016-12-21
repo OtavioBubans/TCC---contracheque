335acda7b20aac86783edb97f5d297d2b5411c69
Matheus Schmitz <augs.math@gmail.com>
2016-12-21 20:59:32 -0200

    feat(Importação): Mover importação para HomeController e remover AdminController.

--- contra-cheque/contra-cheque-web/src/main/java/br/com/crescer/contra/cheque/web/HomeController.java
+++ contra-cheque/contra-cheque-web/src/main/java/br/com/crescer/contra/cheque/web/HomeController.java
@@ -14,21 +14,36 @@ import br.com.crescer.contra.cheque.entity.Usuario;
 import br.com.crescer.contra.cheque.entity.enumeration.TipoOperacaoLog;
 import br.com.crescer.contra.cheque.service.AcessoService;
 import br.com.crescer.contra.cheque.service.EmailService;
+import br.com.crescer.contra.cheque.service.LancamentoService;
 import br.com.crescer.contra.cheque.service.LogService;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import br.com.crescer.contra.cheque.service.ServicoDeDatas;
+import br.com.crescer.contra.cheque.service.exceptions.RegraDeNegocioException;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.stream.Stream;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  *
@@ -51,6 +66,15 @@ public class HomeController {
 
     @Autowired
     HttpServletRequest request;
+    
+    @Autowired
+    LancamentoService lancamentoService;
+    
+    @Autowired
+    Environment environment;
+    
+    @Autowired
+    ServicoDeDatas servicoDeDatas;
 
     @Secured({"ROLE_USER"})
     @RequestMapping("/home")
@@ -58,14 +82,48 @@ public class HomeController {
         registrarAcesso();
         return "home";
     }
+    
+    @RequestMapping("/admin")
+    String admin(Model model) {
+        model.addAttribute("anos", servicoDeDatas.popularAnosAdmin());
+        model.addAttribute("meses", servicoDeDatas.popularMeses());
+        return "admin";
+    }
 
-//      Teste para enviar email    
-//    @RequestMapping("/email")
-//    String envioEmail(){
-//        Email email= new Email("otaviobubans@hotmail.com","12345","1233");
-//        emailService.enviarEmail(email);
-//        return("home");
-//    }
+    @RequestMapping(value = "/admin", method = RequestMethod.POST)
+    String admin(@RequestParam("file") MultipartFile file, String mes, Long ano, RedirectAttributes redirectAttributes) {
+        String nomeArquivo = file.getOriginalFilename();
+        String diretorio = environment.getProperty("upload.arquivo");
+        Path path = Paths.get(diretorio, nomeArquivo);
+        Stream<String> arquivoImportado = null;
+        Date dataDeImportacao = new Date();
+
+        if (file.isEmpty() || file.getSize() == 0) {
+            return "redirect:admin";
+        }
+        if (!nomeArquivo.contains(".txt")) {
+            redirectAttributes.addFlashAttribute("msg", "Errouuuuu");
+            return "redirect:admin";
+        }
+        try (BufferedOutputStream buffer = new BufferedOutputStream(new FileOutputStream(new File(path.toString())))) {
+            buffer.write(file.getBytes());
+            arquivoImportado = Files.lines(path);
+        } catch (IOException e) {
+            return "redirect:admin";
+        }
+
+        try {
+            dataDeImportacao = servicoDeDatas.DataSelecionada(mes, ano);
+            lancamentoService.importarArquivo(arquivoImportado, dataDeImportacao);
+        } catch (RegraDeNegocioException ex) {
+            redirectAttributes.addFlashAttribute("msg", ex.getMessage());
+            return "redirect:admin";
+        }
+
+        redirectAttributes.addFlashAttribute("msg", "Arquivo importado com sucesso");
+        return "admin";
+    }
+
     private void registrarAcesso() {
         Colaborador colaboradorLogado = usuarioLogado().getColaborador();
         Calendar c = new GregorianCalendar();
@@ -136,7 +194,6 @@ public class HomeController {
         usuarioLogado.setLoginsSuspeitos(loginsSuspeitos + 1);
         usuarioService.save(usuarioLogado);
         if (loginsSuspeitos == 1) {
-
             Email novoEmail = new Email(usuarioLogado.getEmail(), ip, usuarioLogado().getColaborador().getNome());
             emailService.enviarEmail(novoEmail);
         }
@@ -156,7 +213,7 @@ public class HomeController {
                 ip = request.getRemoteAddr();
             }
         }
-        if(ip.equals("0:0:0:0:0:0:0:1")){
+        if (ip.equals("0:0:0:0:0:0:0:1")) {
             ip = "127.0.0.1";
         }
         return ip;
