e83c80fdbb0e304975d173655424c894b0fa103f
Otávio Bubans <otaviobubans@hotmail.com>
2016-12-22 12:49:19 -0200

    Fix(Email):Corpo email

--- contra-cheque/contra-cheque-entity/src/main/java/br/com/crescer/contra/cheque/entity/Email.java
+++ contra-cheque/contra-cheque-entity/src/main/java/br/com/crescer/contra/cheque/entity/Email.java
@@ -17,9 +17,36 @@ public class Email {
         this.assunto = "EMAIL SEGURANÇA CONTRACHEQUE";
         this.destinatario = destinatario;
         this.remetente = "contracheque.cwi@gmail.com"; // email do admin;
-        this.texto = "Houve uma incorfomidade suspeita no acesso do contracheque.   "
-                    + "     Informações do IP: "+ip
-                    + "     Colaborador: "+usuario;
+        this.texto = "<html>\n" +
+                            "<head>\n" +
+                            "    \n" +
+                            "</head>\n" +
+                            "<body >\n" +
+                            "    <div style=\"width:450px; margin:10px auto; text-align: justify;\">\n" +
+                            "        <br/> \n" +
+                            "        <br/>\n" +
+                            "\n" +
+                            "        <h4>Alerta</h4>\n" +
+                            "\n" +
+                            "        <br/>\n" +
+                            "\n" +
+                            "        <p>Houve uma incorfomidade suspeita no acesso do contracheque no sistema de visualização dos contracheques.</p>\n" +
+                            "        <p>Alguém está acessando o contracheque com algum IP ou horário fora do comum do colaborador<b> " +usuario + " </b>.</p>\n" +
+                            "\n" +
+                            "        <br/>\n" +
+                            "\n" +
+                            "        <p><b>IP do maquina de acesso: </b> <label> " + ip +  " </label></p>\n" +
+                            "        <br/> \n" +
+                            "\n" +
+                            "        <p>Entre em contato com o usuário para fazer a verificação de acesso incomum!</p>\n" +
+                            "        <div style=\"width:250px;text-align:center;margin:0px auto;\">\n" +
+                            "        <img src=\"http://i.imgur.com/92ei8XC.png\" width=\"250px\" alt=\"CWI\"/><h3> wwww.cwi.com.br</h3>\n" +
+                            "        </div>          \n" +
+                            "    </div>\n" +
+                            "              \n" +
+                            "    \n" +
+                            "</body>\n" +
+                            "</html>";
          
     }
     
