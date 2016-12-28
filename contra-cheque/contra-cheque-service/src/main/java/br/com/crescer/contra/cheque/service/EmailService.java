270d4813012cdb611a0d3ad7629b172086998332
Mateus Teixeira <mateusbpt@yahoo.com.br>
2016-12-27 23:10:22 -0200

    refactor(contra-cheque-service): adicionar login e senha do serviço de e-mail a partir de variáveis de ambiente.

--- contra-cheque/contra-cheque-service/src/main/java/br/com/crescer/contra/cheque/service/EmailService.java
+++ contra-cheque/contra-cheque-service/src/main/java/br/com/crescer/contra/cheque/service/EmailService.java
@@ -1,9 +1,7 @@
-
 package br.com.crescer.contra.cheque.service;
 
 import br.com.crescer.contra.cheque.entity.Email;
 import org.springframework.stereotype.Service;
-
 import java.util.Properties;
 import javax.mail.Address;
 import javax.mail.Message;
@@ -14,50 +12,46 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-
-/**
- *
- * @author Otávio
- */
 @Service
 public class EmailService {
-        
-        
-	public void enviarEmail(Email email) {
 
-         Properties props = new Properties();
-           
-            props.put("mail.smtp.host", "smtp.gmail.com");
-            props.put("mail.smtp.socketFactory.port", "465");
-            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-            props.put("mail.smtp.auth", "true");
-            props.put("mail.smtp.port", "465");
+    public void enviarEmail(Email email) {
 
-            Session session = Session.getDefaultInstance(props,
-                        new javax.mail.Authenticator() {
-                             protected PasswordAuthentication getPasswordAuthentication() 
-                             {
-                                   return new PasswordAuthentication("contracheque.cwi@gmail.com", "crescer16");
-                             }
-                        });
-           
-            session.setDebug(true);
-            try {
+        Properties props = new Properties();
 
-                  Message message = new MimeMessage(session);
-                  message.setFrom(new InternetAddress(email.getEmailAdmin())); 
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
 
-                  Address[] toUser = InternetAddress 
-                             .parse(email.getDestinatario());  
-                  message.setRecipients(Message.RecipientType.TO, toUser);
-                  message.setSubject(email.getAssunto());
-                  message.setContent(email.getTexto(), "text/html; charset=utf-8");
-                
-                  Transport.send(message);
-             } catch (MessagingException e) {
-                  throw new RuntimeException(e);
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                String email = System.getenv().get("EMAIL_LOGIN");
+                String senha = System.getenv().get("EMAIL_SENHA");
+                return new PasswordAuthentication(email, senha);
             }
-    
-	} 
+        });
+
+        session.setDebug(true);
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(email.getEmailAdmin()));
+
+            Address[] toUser = InternetAddress
+                    .parse(email.getDestinatario());
+            message.setRecipients(Message.RecipientType.TO, toUser);
+            message.setSubject(email.getAssunto());
+            message.setContent(email.getTexto(), "text/html; charset=utf-8");
+
+            Transport.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
 }
